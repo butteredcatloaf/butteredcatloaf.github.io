bb7d147e78821561a5f98aebacd89fe90d0297a9
butteredcatloaf <elizabethjpzhang@gmail.com>
2023-02-18 15:15:15 -0500

    test.html added

---
+++ navbar.js
@@ -0,0 +1,10 @@
+var navbar = document.querySelector('nav-bar');
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
